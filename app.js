f199509be5935435f1cb95cac250290169233033
Matthew Baseman <matthew.baseman@gmail.com>
2023-04-10 11:35:21 -0700

    GroceryList which bolds when item is hovered over

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // TODO
+const {useState} = React;
+
+const GroceryListItem = (props) => {
+  const [inCart, setInCart] = useState(false);
+  const style = {
+    fontWeight: inCart ? 'bold' : 'normal'
+  };
+  return (
+    <li style={style} onMouseEnter={() => setInCart(!inCart)} onMouseLeave={() => setInCart(!inCart)}>
+      {props.grocery}
+    </li>
+  );
+};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.groceries.map((grocery) => (
+      <GroceryListItem grocery={grocery} />
+    ))}
+  </ul>
+);
+
+
+
+const App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceries={['Milk', 'Chicken Thighs', 'Cucumber']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
